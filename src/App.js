0533d0d43adf4dddbd83fdb35833214d02c984a9
franncottet <cottetfranco@gmail.com>
2022-09-17 12:55:53 -0300

    creando routing

--- src/App.js
+++ src/App.js
@@ -2,14 +2,26 @@ import React from "react";
 import ItemDetailContainer from "./components/ItemDetailContainer/ItemDetailContainer.js";
 import ItemListContainer from "./components/ItemListContainer.js";
 import Navbar from "./components/Navbar.js";
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import Cart from "./components/Cart/Cart.js";
 
 
 const App = () => {  
     return (
         <>
+       <BrowserRouter>
             <Navbar/>
-            <ItemListContainer/>
-            <ItemDetailContainer/>
+            <Routes>
+              <Route path="/" element={<ItemListContainer/>} />
+              <Route path="/categoria" element={<ItemListContainer/>} />
+              <Route path="/cart" element={<Cart/>} />
+              <Route path="/categoria" element={<ItemDetailContainer/>} />
+            </Routes>
+            
+            
+            
+        
+        </BrowserRouter>
          </>
     )
  }
